8717e5cde5f843141026a9764616a8f0295655b3
mgabrielef <mariagabrieleoliveira@gmail.com>
2024-07-17 22:01:02 -0300

    feat: verificacao de cpf valido

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,30 +1,62 @@
 /// <reference types="cypress"/>
+import { generate } from 'gerador-validador-cpf'
 
 describe('Validação da função de cadastro', () => {
 
   beforeEach(()=>{
     cy.visit('/')
+    cy.get('[data-cy="button-btn-enroll"]').click()
   })
 
   it('Deve realizar o cadastro com sucesso', () => {
     //let email = `email${Math.floor(Math.random() * 100000000)}@mail.com`
-    //cy.cadastroDadosPessoais('nome', 'sobremome', '23042000', email, 'teste')
+    //cy.cadastroDadosPessoais('nome', 'sobrenome', '23042000', email, 'teste')
     //cy.cadastroEndereco()
     cy.cadastroCompleto()
     cy.get('.text-lime-500').should('contain', 'Thank you for')
   });
 
-  it('Deve mostrar mensagem de erro ao tentar cadastrar com email invalido', () => {
-    cy.cadastroDadosPessoais('nome', 'sobremome', '23042000', 'email', 'teste')
+  it('Deve mostrar mensagem de erro ao tentar cadastrar com email inválido', () => {
+    cy.cadastroDadosPessoais('nome', 'sobrenome', '23042000','email', 'senha')
     cy.get(':nth-child(1) > .form-container > .input-error').should('contain', 'Email inválido')
   });
 
-  it.only('Deve mostrar mensagem de erro ao tentar cadastrar com email já cadastrado', () => {
+  it('Deve mostrar mensagem de erro ao tentar cadastrar com email já cadastrado', () => {
     cy.cadastroCompleto().then(()=>{
+      cy.wait(3000)
       let email = Cypress.env('generatedEmail')
       cy.visit('/')
-      cy.cadastroDadosPessoais('nome', 'sobremome', '23042000', email, 'senha')
+      cy.cadastroDadosPessoais('nome', 'sobrenome', '23042000', email, 'senha')
       cy.get('.input-error').should('contain', 'Este email já está em uso')
     })
   });
+
+  it('Deve mostrar mensagem de erro ao tentar cadastrar com CPF inválido', ()=>{
+    let email = `email${Math.floor(Math.random() * 100000000)}@mail.com`
+    cy.cadastroDadosPessoais('nome', 'sobrenome', '23042000', email, 'senha', '12345678910')
+    cy.get('.input-error').should('contain', 'CPF inválido')
+  })
+
+  it('Deve mostrar mensagem de erro ao tentar cadastrar com CPF já cadastrado', () => {
+    cy.cadastroCompleto().then(()=>{
+      cy.wait(3000)
+      let email = `email${Math.floor(Math.random() * 100000000)}@mail.com`
+      let cpf = Cypress.env('generatedCPF')
+      cy.visit('/')
+      cy.cadastroDadosPessoais('nome', 'sobrenome', '23042000', email, 'senha', cpf)
+      cy.get('.input-error').should('contain', 'Este CPF já está em uso')
+    })
+  });
+
+  it('Deve mostrar mensagem de erro ao tentar cadastrar com CEP inválido', () => {
+    let email = `email${Math.floor(Math.random() * 100000000)}@mail.com`
+    cy.cadastroDadosPessoais('nome', 'sobrenome', '23042000', email, 'senha')
+    cy.cadastroEndereco('12345678', '23')
+    cy.get('.toasts-list').should('contain', 'CEP não encontrado')
+  });
+  
+  it.only('Deve mostrar mensagem de que o campo Nome é obrigatório', () => {
+    cy.get('[data-cy="input-signup-personal-data-firstName"]').should('have.attr', 'required')
+  });
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,11 @@
 import { generate } from 'gerador-validador-cpf'
 
-Cypress.Commands.add('cadastroDadosPessoais', (nome, sobremome, dataNascimento, email, senha)=>{
-    cy.get('[data-cy="button-btn-enroll"]').click()
+Cypress.Commands.add('cadastroDadosPessoais', (nome, sobrenome, dataNascimento, email, senha, cpf)=>{
+    cpf = cpf || generate();
     cy.get('[data-cy="input-signup-personal-data-firstName"]').type(nome)
-    cy.get('[data-cy="input-signup-personal-data-lastName"]').type(sobremome)
+    cy.get('[data-cy="input-signup-personal-data-lastName"]').type(sobrenome)
     cy.get('[data-cy="input-signup-personal-data-birthDate"]').type(dataNascimento)
-    cy.get('[data-cy="input-signup-personal-data-cpf"]').type(generate())
+    cy.get('[data-cy="input-signup-personal-data-cpf"]').type(cpf)
     cy.get('[data-cy="input-signup-personal-data-email"]').type(email)
     cy.get('[data-cy="input-signup-personal-data-email-confirm"]').type(email)
     cy.get('[data-cy="input-signup-personal-data-password"]').type(senha)
@@ -16,9 +16,9 @@ Cypress.Commands.add('cadastroDadosPessoais', (nome, sobremome, dataNascimento, 
     cy.get('[data-cy="button-signup_submit_button_1"]').click()
 })
 
-Cypress.Commands.add('cadastroEndereco', ()=>{
-    cy.get('[data-cy="input-signup-address-cep"]').type('13064759')
-    cy.get('[data-cy="input-signup-address-number"]').type('23')
+Cypress.Commands.add('cadastroEndereco', (cep, numero)=>{
+    cy.get('[data-cy="input-signup-address-cep"]').type(cep)
+    cy.get('[data-cy="input-signup-address-number"]').type(numero)
     cy.wait(1000)
     cy.get('[data-cy="button-signup_submit_button_3"]').click()
 })
@@ -26,6 +26,8 @@ Cypress.Commands.add('cadastroEndereco', ()=>{
 Cypress.Commands.add('cadastroCompleto', ()=>{
     let email = `email${Math.floor(Math.random() * 100000000)}@mail.com`
     Cypress.env('generatedEmail', email)
-    cy.cadastroDadosPessoais('nome', 'sobremome', '23042000', email, 'senha')
-    cy.cadastroEndereco()
+    let cpf = generate()
+    Cypress.env('generatedCPF', cpf)
+    cy.cadastroDadosPessoais('nome', 'sobrenome', '23042000', email, 'senha', cpf)
+    cy.cadastroEndereco('13064759', '23')
 })
